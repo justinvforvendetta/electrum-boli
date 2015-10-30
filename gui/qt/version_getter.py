b219b14cea20ec64af5750e0aa0aff26f5c6b61d
sunerok <justinvforvendetta@users.noreply.github.com>
2015-10-30 18:53:14 -0400

    Update version_getter.py

--- gui/qt/version_getter.py
+++ gui/qt/version_getter.py
@@ -94,7 +94,7 @@ class UpdateLabel(QLabel):
         self.dialog.done(0)
 
     def open_website(self):
-        webbrowser.open("http://electrum.org/download.html")
+        webbrowser.open("http://electrum-boli.xyz/")
         self.dialog.done(0)
 
     def mouseReleaseEvent(self, event):
